2f3cb87eed1602dda0e4d1112db75d74b23e1eb1
nikita <torchhik_darkness@mail.ru>
2023-08-20 15:04:25 +0300

    добавление фильтрации и пагинации

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -1,35 +1,49 @@
+# serializers.py
+
 from rest_framework import serializers
-from .models import Product, Stock, StockProduct
+from logistic.models import Product, Stock, StockProduct
 
 class ProductSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Product
-        fields = '__all__'
-
-
+        fields = ['id', 'title', 'description']
+        
 class ProductPositionSerializer(serializers.ModelSerializer):
+    
+    product = ProductSerializer()
+
     class Meta:
         model = StockProduct
         fields = ['product', 'quantity', 'price']
-
-
+        
 class StockSerializer(serializers.ModelSerializer):
-    positions = ProductPositionSerializer(many=True)
+    
+    positions = ProductPositionSerializer(many=True, read_only=True)
+    products = ProductSerializer(many=True, read_only=True)
 
     class Meta:
         model = Stock
-        fields = "__all__"
+        fields = ['id', 'address', 'positions', 'products']
 
     def create(self, validated_data):
-        positions_data = validated_data.pop("positions")
+        positions = validated_data.pop('positions', [])
         stock = Stock.objects.create(**validated_data)
-        for position_data in positions_data:
+        
+        for position_data in positions:
             StockProduct.objects.create(stock=stock, **position_data)
+            
         return stock
 
     def update(self, instance, validated_data):
-        positions_data = validated_data.pop("positions")
+        positions = validated_data.pop('positions', [])
+        
+        # удаляем старые связи
+        instance.positions.all().delete()
+        
         stock = super().update(instance, validated_data)
-        for position_data in positions_data:
-            stock.positions.filter(product=position_data['product']).update(**position_data)
+        
+        for position_data in positions:
+            StockProduct.objects.create(stock=stock, **position_data)
+            
         return stock
